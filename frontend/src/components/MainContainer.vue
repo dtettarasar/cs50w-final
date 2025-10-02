a9ac73f3edad5cbf73faa23c4334338cc4b3e51d
Dylan Tettarasar <dtettarasar@gmail.com>
2025-10-02 09:12:25 +0000

    remove initial elements from Vue JS

---
+++ frontend/src/components/MainContainer.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <h1 class="text-3xl font-bold underline"> Hello world! </h1>
+
+    <HelloFromBackend />
+
+    <div class="text-white m-4 p-4 bg-emerald-950 font-bold rounded-lg">
+        <p>Please be careful when feeding the birds.</p>
+    </div>
+
+</template>
