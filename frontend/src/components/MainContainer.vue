b55c3215651b3f8723b8bae85b2c7d5f8d62ceea
Dylan Tettarasar <dtettarasar@gmail.com>
2025-10-02 09:25:28 +0000

    built generic navbar component

--- frontend/src/components/MainContainer.vue
+++ frontend/src/components/MainContainer.vue
@@ -1,8 +1,12 @@
 <script setup>
+    
+    import NavBar from './common/NavBar.vue';
 
 </script>
 
 <template>
+
+    <NavBar/>
 
     <h1 class="text-3xl font-bold underline"> Hello world! </h1>
 
---
+++ frontend/src/components/common/NavBar.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <div>
+
+        <nav class="bg-gray-800 p-4 text-white">
+            <ul class="flex space-x-4">
+                <li><a href="#" class="hover:underline">Home</a></li>
+                <li><a href="#" class="hover:underline">About</a></li>
+                <li><a href="#" class="hover:underline">Services</a></li>
+                <li><a href="#" class="hover:underline">Contact</a></li>
+            </ul>
+        </nav>
+        
+    </div>
+
+</template>
