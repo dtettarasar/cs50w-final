2dfae58b856478abda6bea8bcb2c538d1e5cae4c
Dylan Tettarasar <dtettarasar@gmail.com>
2025-10-12 11:10:35 +0200

    Vue: update the router to add new routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router"
 import Home from "../views/Home.vue"
+import Contact from "../views/Contact.vue"
+import Login from "../views/Login.vue"
+import Register from "../views/Register.vue"
 
 const router = createRouter({
   history: createWebHistory("/"),
@@ -9,6 +12,21 @@ const router = createRouter({
       name: 'home',
       component: Home
     },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    }
   ],
 })
 
